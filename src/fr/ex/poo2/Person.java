496747ed4beec1ec01721f83d561dc05e1356d9a
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-12 14:55:36 +0100

    ajout de la javadoc

--- src/fr/ex/poo2/Person.java
+++ src/fr/ex/poo2/Person.java
@@ -1,17 +1,39 @@
 package fr.ex.poo2;
 
+/**
+ * Represents a person with basic personal information such as name, age, address, and birth city
+ */
 public class Person {
 	
 	// ==== Attributes===
+	// Personal informations
 	
+	/** The person's first name */
 	private String firstName;
+	
+	/** The person's last name */
 	private String lastName;
+	
+	/** The person's age */
 	private int age;
+	
+	/** The person's address */
 	private String address;
+	
+	/** The city where the person was born */
 	private City birthCity;
 	
 	// == Constructors ==
 	
+	/**
+     * Constructs a Person with full details
+     *
+     * @param firstName the person's first name
+     * @param lastName the person's last name
+     * @param age the person's age
+     * @param address the person's address
+     * @param birthCity the city where the person was born
+     */
 	public Person(String firstName, String lastName, int age, String address, City birthCity) {
 		setFirstName(firstName);
 		setLastName(lastName);
@@ -20,6 +42,13 @@ public class Person {
 		this.birthCity = birthCity;	
 	}
 	
+	 /**
+     * Constructs a Person with name and age, default address is "Inconnue"
+     *
+     * @param firstName the person's first name
+     * @param lastName the person's last name
+     * @param age the person's age
+     */
 	public Person(String firstName, String lastName, int age) {
 		setFirstName(firstName);
 		setLastName(lastName);
@@ -27,6 +56,12 @@ public class Person {
 		this.address = "Inconnue";
 	}
 	
+	/**
+     * Constructs a Person with only name, default age is 0 and address is "Inconnue"
+     *
+     * @param firstName the person's first name
+     * @param lastName the person's last name
+     */
 	public Person(String firstName, String lastName) {
 		setFirstName(firstName);
 		setLastName(lastName);
@@ -36,29 +71,56 @@ public class Person {
 	
 	// -- Accessors--
 	
-	// For the first name
+	/**
+     * Gets the person's first name
+     *
+     * @return the first name
+     */
 	public String getFirstName() {
 		return firstName;
 	}
 	
+	 /**
+     * Sets the person's first name
+     *
+     * @param firstName the first name to set
+     */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 	
-	// For the last name
+	/**
+     * Gets the person's last name
+     *
+     * @return the last name
+     */
 	public String getLastName() {
 		return lastName;
 	}
 	
+	/**
+     * Sets the person's last name
+     *
+     * @param lastName the last name to set
+     */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 	
-	// For the age (with validation)
+	/**
+     * Gets the person's age
+     *
+     * @return the age
+     */
 	public int getAge() {
 		return age;
 	}
 	
+	/**
+     * Sets the person's age. If the age is negative, sets it to -1
+     *
+     * @param age the age to set
+     */
 	public void setAge(int age) {
 		if (age < 0) {
 			this.age = -1;
@@ -69,16 +131,29 @@ public class Person {
 		
 	}
 	
-	// For the address
+	   /**
+     * Gets the person's address.
+     *
+     * @return the address
+     */
 	public String getAddress() {
 		return address;
 	}
 	
+	/**
+     * Sets the person's address.
+     *
+     * @param address the address to set
+     */
 	public void setAddress(String address) {
 		this.address = address;
 	}
 	
-	// for the birth city
+	/**
+     * Gets the person's birth city.
+     *
+     * @return the birth city
+     */
 	public City getBirthCity() {
         return birthCity;
     }
@@ -87,10 +162,13 @@ public class Person {
         this.birthCity = birthCity;
     }
 
-	
-	
-	// methods
-	
+    // == Methods ==
+    
+    /**
+     * Returns a string representation of the person's details.
+     *
+     * @return a formatted string with personal information
+     */
     public String toString() {
         String result = "Prénom: " + getFirstName() + "\n" +
                         "Nom: " + getLastName() + "\n" +
