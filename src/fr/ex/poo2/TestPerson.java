5c46ace2ac01c8c8008056aabd8484fa7b735cb3
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-12 14:15:34 +0100

    ajout d'un test d'affichage de ville

--- src/fr/ex/poo2/TestPerson.java
+++ src/fr/ex/poo2/TestPerson.java
@@ -6,13 +6,19 @@ public class TestPerson {
 	
 		// New Person
 		Person manu = new Person("Emmanuel","Macron", 47, "Elysée", new City("France", "Amiens"));
-		Person mor = new Person("Mor", "Diop", 25);
-		Person potter = new Person("Harry", "Potter");
+		Person potter = new Person("Harry", "Potter", 12, "Poudlard", new City("Royaume-Uni", "Le placard sous l'escalier", 9000000 ));
+		Person sarko = new Person("Nicolas", "Sarkozy", 70, "Prison de la Santé", new City("France","Paris"));
+		Person theo = new Person("Theo", "Sahrane", 25, "[Censored]", new City("France", "Dans un chou"));
+		
+		City paris = new City("France","Toulouse");
 		
 		// Display
 		System.out.println(manu);
-		System.out.println(mor);
 		System.out.println(potter);
+		System.out.println(sarko);
+		System.out.println(theo);
+		
+		System.out.println(paris);
 		
 	}
 
